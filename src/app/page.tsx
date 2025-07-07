d80d3fc24a8875da7be9f9be7cd2632befd68617
rogermarco <marcrogers90@gmail.com>
2025-07-07 22:30:06 +0100

    try some new styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,23 +6,23 @@ export default async function Home() {
  
   return (
     <div className="w-[90%] md:w-4/5 flex flex-col items-center justify-center m-auto tracking-wide">
-      <div className="flex flex-col items-center justify-center w-[90%] md:w-4/5 text-center my-3">
+      <div className="flex flex-col items-center justify-center w-[90%] mt-4 md:w-4/5 text-center bg-[#FFBA49] border-3 rounded-3xl shadow-[4px_4px_0px_#111] p-3 md:p-6">
         <h1 className="font-semibold text-2xl">Vindico Arena Ice Skating Sessions</h1>
-        <p>
+        <p className="p-2 md:p-4">
           The Vindico website for figuring out session times is bad, so I made this instead. 
           Pulls data directly from their sources so you can trust it is up to date.
           Will only show the dates that Vindico has scheduled for.
         </p>
       </div>
-      <hr className="mx-auto w-3/4 border-b-3 border-neutral-300 mb-3" />
-      <div className="sessions-container grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 border-t-1 border-l-1">
+      <hr className="mx-auto w-3/4 border-4 border-[#6C9A8B] shadow-[4px_4px_0px_#111] my-6" />
+      <div className="sessions-container grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 md:gap-4">
         {Object.entries(sessions).length > 0 ? (
           Object.entries(sessions).map(([dateString, sessionTypes]) => {
             // Convert the date string to a Date object for formatting
             const date = new Date(dateString);
             
             return (
-              <div key={format(date, 'MMMM d')} className="date-section odd:bg-fuchsia-100 even:bg-purple-200 text-center p-1 border-r-1 border-b-1">
+              <div key={format(date, 'MMMM d')} className="odd:bg-fuchsia-100 even:bg-purple-200 text-center p-1 border-[#111] border-2 md:border-3 rounded-3xl shadow-[4px_4px_0px_#111] md:shadow-[8px_8px_0px_#111]">
                 <h2 className="font-semibold text-base sm:text-xl text-balance">{format(date, 'EEEE MMMM d')}</h2>
                 <div className="session-types-container text-sm sm:text-base">
                   <div className="session-type">
@@ -62,7 +62,7 @@ export default async function Home() {
         )}
       </div>
       <footer className="mt-2">
-        <p className="font-light">Made by Marc with ❤️</p>
+        <p className="font-light my-4">Made by Marc with ❤️</p>
       </footer>
     </div>
   );
